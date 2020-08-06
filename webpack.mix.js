20c41f7e0bc5b2d9ef99deab6024b7d1d70a2adc
Tássio Caique <tassiok2@gmail.com>
2020-08-06 14:46:35 -0300

    Add - Configurações do vuetify

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,5 +24,34 @@ const mix = require('laravel-mix');
 |
 */
 
+module.exports = {
+  rules: [
+    {
+      test: /\.s(c|a)ss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          // Requires sass-loader@^7.0.0
+          options: {
+            implementation: require('sass'),
+            fiber: require('fibers'),
+            indentedSyntax: true // optional
+          },
+          // Requires sass-loader@^8.0.0
+          options: {
+            implementation: require('sass'),
+            sassOptions: {
+              fiber: require('fibers'),
+              indentedSyntax: true // optional
+            },
+          },
+        },
+      ],
+    },
+  ],
+}
+
 mix.js('resources/js/modules/home/app.js', 'public/js/app.min.js')
 	.sass('resources/sass/app.scss', 'public/css');
