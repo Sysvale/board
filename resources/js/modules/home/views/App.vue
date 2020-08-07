47d43b3741a87103ac2348d9b152f15a3cc720a3
Tássio Caique <tassiok2@gmail.com>
2020-08-06 23:57:31 -0300

    Add - quicksand como font-family

--- resources/js/modules/home/views/App.vue
+++ resources/js/modules/home/views/App.vue
@@ -6,3 +6,10 @@
 export default {
 }
 </script>
+<style scoped>
+.v-application {
+	background-color: #F4F4F6;
+	font-family: 'Quicksand', sans-serif;
+	font-weight: 500;
+}
+</style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	@yield('css')
 </head>
