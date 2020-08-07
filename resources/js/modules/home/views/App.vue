1a98fd7dc4c39d9ff08052e416b53bb63402d7e0
Tássio Caique <tassiok2@gmail.com>
2020-08-06 23:58:19 -0300

    Ref - barra superior

--- resources/js/modules/home/views/App.vue
+++ resources/js/modules/home/views/App.vue
@@ -1,5 +1,17 @@
 <template>
-	<router-view />
+	<v-app>
+		<v-app-bar
+			app
+			color="#333"
+			dense
+			dark
+		>
+			<v-toolbar-title>Trelássio</v-toolbar-title>
+		</v-app-bar>
+		<v-main>
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
