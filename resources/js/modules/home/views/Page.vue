9541db15c24ae235ab0263af17dfe701a2fe3a96
Tássio Caique <tassiok2@gmail.com>
2020-08-07 00:03:36 -0300

    Add - boilerplate da  planning page

--- resources/js/modules/home/views/Page.vue
+++ resources/js/modules/home/views/Page.vue
@@ -1,16 +1,98 @@
 <template>
-	<div class="d-flex h-100">
-		<div class="container flex-grow-1 w-30 mt-3">
-			<div class="p-4">
-				<h3 class="mb-4">Pagina teste</h3>
-			</div>
-		</div>
-	</div>
+	<v-container
+		grid-list-md
+		fluid
+	>
+		<h2 class="mb-3 text-center font-weight-light">Planning Board</h2>
+		<v-divider class="py-3"/>
+		<v-layout
+			colum
+		>
+			<list-container>
+				<list
+					:title="getListName(DEV_LOG)"
+					:list="getList(DEV_LOG)"
+					:group="boardContext"
+				/>
+				<list
+					:title="getListName(BUGS)"
+					:list="getList(BUGS)"
+					:group="boardContext"
+					class="mt-10"
+				/>
+			</list-container>
+			<list-container>
+				<list
+					:title="getListName(BACKLOG)"
+					:list="getList(BACKLOG)"
+					:group="boardContext"
+				/>
+			</list-container>
+			<list-container>
+				<list
+					:title="getListName(TEAM_ONE)"
+					:list="getList(TEAM_ONE)"
+					:group="boardContext"
+				/>
+			</list-container>
+			<list-container>
+				<list
+					:title="getListName(TEAM_TWO)"
+					:list="getList(TEAM_TWO)"
+					:group="boardContext"
+				/>
+			</list-container>
+		</v-layout>
+	</v-container>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import makeFormFields from '../../../core/utils/makeFormFields';
+import List from '../components/List.vue';
+import ListContainer from '../components/ListContainer.vue';
+import { PLANNING_BOARD } from '../constants/defaultBoards';
+import {
+	BACKLOG,
+	TEAM_ONE,
+	TEAM_TWO,
+	DEV_LOG,
+	BUGS,
+} from '../constants/defaultLists';
 
 export default {
+	components: {
+		List,
+		ListContainer,
+	},
 
+	data() {
+		return {
+			BACKLOG,
+			TEAM_ONE,
+			TEAM_TWO,
+			DEV_LOG,
+			BUGS,
+		};
+	},
+
+	computed: {
+		...makeFormFields(
+			'planning',
+			PLANNING_BOARD.map(({key}) => key)
+		),
+		boardContext() {
+			return 'planning';
+		},
+	},
+
+	methods: {
+		getListName(boardKey) {
+			return PLANNING_BOARD.filter(({ key }) => key === boardKey)[0].name;
+		},
+		getList(boardKey) {
+			return this[boardKey];
+		},
+	}
 }
 </script>
