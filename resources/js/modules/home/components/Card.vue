551f37eee5d1d331c6566b237ee891229d913eb1
Tássio Caique <tassiok2@gmail.com>
2020-08-07 00:00:39 -0300

    Add - boilerplate card

---
+++ resources/js/modules/home/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+	<v-card
+		class="task-card"
+		v-bind="$attrs"
+		v-on="$listeners"
+		hover
+	>
+		<slot />
+	</v-card>
+</template>
+<style scoped>
+.task-card {
+	box-shadow: 0 3px 1px -2px rgba(0,0,0,.05),
+		0 2px 2px 0 rgba(0,0,0,.05),
+		0 1px 5px 0 rgba(0,0,0,.05) !important;
+}
+</style>
