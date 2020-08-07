60c786c5017a1383b7b15cd4002f54780e71d607
Tássio Caique <tassiok2@gmail.com>
2020-08-07 00:00:28 -0300

    Add - boilerplate componente de container para as listas

---
+++ resources/js/modules/home/components/ListContainer.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-container
+		fluid
+		justify-start
+		align-content-start
+		class="list-container"
+	>
+		<slot />
+	</v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.list-container {
+	max-width: 275px;
+	margin-right: 0px!important;
+	margin-left: 0px!important;
+}
+</style>
