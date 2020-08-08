9c17be4b61fba0dede1f4e0cc154c1818fc616e9
Tássio Caique <tassiok2@gmail.com>
2020-08-08 00:07:22 -0300

    Add - ajustes de estilização no card

--- resources/js/modules/board/components/Card.vue
+++ resources/js/modules/board/components/Card.vue
@@ -5,7 +5,11 @@
 		v-on="$listeners"
 		hover
 	>
-		<slot />
+		<v-card-text
+			class="text--black"
+		>
+			<slot />
+		</v-card-text>
 	</v-card>
 </template>
 <style scoped>
@@ -13,5 +17,7 @@
 	box-shadow: 0 3px 1px -2px rgba(0,0,0,.05),
 		0 2px 2px 0 rgba(0,0,0,.05),
 		0 1px 5px 0 rgba(0,0,0,.05) !important;
+	max-width: 250px;
+	min-width: 250px;
 }
 </style>
