2c48b5e2477e16a0cd3b6989446f916191420d08
Tássio Caique <tassiok2@gmail.com>
2020-08-08 14:55:35 -0300

    Add - Botão de exclusão e eventos de mousover e mouseleve

--- resources/js/modules/board/components/Card.vue
+++ resources/js/modules/board/components/Card.vue
@@ -4,14 +4,38 @@
 		v-bind="$attrs"
 		v-on="$listeners"
 		hover
+		@mouseover="hover = true"
+		@mouseleave="hover = false"
 	>
 		<v-card-text
 			class="text--black"
 		>
 			<slot />
 		</v-card-text>
+		<v-card-actions
+			v-if="hover"
+		>
+			<v-btn
+				block
+				outlined
+				color="red"
+				small
+				@click="$emit('delete')"
+			>
+				Excluir
+			</v-btn>
+		</v-card-actions>
 	</v-card>
 </template>
+<script>
+export default {
+	data() {
+		return {
+			hover: false,
+		};
+	},
+}
+</script>
 <style scoped>
 .task-card {
 	box-shadow: 0 3px 1px -2px rgba(0,0,0,.05),
