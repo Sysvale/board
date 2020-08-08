cfd25e411943c2f791aacdcdc6df3273c6db562d
Tássio Caique <tassiok2@gmail.com>
2020-08-08 00:08:00 -0300

    Add - List container dentro do list de handle para adicionar um novo card a lista

--- resources/js/modules/board/components/List.vue
+++ resources/js/modules/board/components/List.vue
@@ -1,57 +1,76 @@
 <template>
-	<div>
-		<div class="mb-4 text--black">
-			<span class="mb-0 font-weight-bold d-block">
-				{{ title }}
-			</span>
-			<div class="mt-n2">
-				<small class="mt-n2">
-					{{ cardsQuantity }}
-				</small>
+	<list-container>
+		<header class="mb-4 text--black">
+			<div>
+				<span class="mb-0 font-weight-bold d-block">
+					{{ title }}
+				</span>
+				<div class="mt-n2">
+					<small class="mt-n2">
+						{{ cardsQuantity }}
+					</small>
+				</div>
 			</div>
-		</div>
+		</header>
+		<v-btn
+			outlined
+			block
+			small
+			class="mb-2"
+			:disabled="createMode"
+			@click="createMode = true"
+		>
+			<v-icon>add</v-icon>
+		</v-btn>
+		<v-textarea
+			v-if="createMode"
+			v-model="newCardTitle"
+			solo
+			auto-grow
+			autofocus
+			@blur="save"
+			@keydown.enter="save"
+			@keydown.esc="clear"
+		/>
 		<draggable
 			v-bind="$attrs"
-			@start="drag=true"
-			@end="drag=false"
-			:class="{
-				'd-flex': horizontal,
-			}"
+			v-on="$listeners"
+			:key="`${$attrs.id}-${$attrs.list.length}`"
 		>
 				<card
 					v-for="(element, i) in $attrs.list"
-					:key="element.id + i"
+					:key="element.id "
 					:class="{
-						'mt-2': i > 0 && !horizontal,
-						'ml-2': i > 0 && horizontal,
-						'horizontal': horizontal,
+						'mt-2': i > 0,
 					}"
 				>
-					<v-card-text
-						class="text--black"
-					>
-						{{ element.title }}
-					</v-card-text>
+					{{ element.title }}
 				</card>
 		</draggable>
-	</div>
+	</list-container>
 </template>
 
 <script>
 import Card from './Card';
+import ListContainer from './ListContainer';
+
 export default {
 	components: {
 		Card,
+		ListContainer,
 	},
 	props: {
 		title: {
 			type: String,
 			default: 'title',
 		},
-		horizontal: {
-			type: Boolean,
-			default: false,
-		}
+	},
+
+	data() {
+		return {
+			newCardTitle: null,
+			createMode: false,
+		};
 	},
 
 	computed: {
@@ -66,11 +85,23 @@ export default {
 			return `${length} cartões`;
 		}
 	},
+
+	methods: {
+		save() {
+			if(this.newCardTitle && this.newCardTitle.trim() !== '') {
+				this.$emit('save', {
+					title: this.newCardTitle,
+					status: this.$attrs.id,
+				});
+				this.newCardTitle = null;
+			}
+			this.createMode = false;
+		},
+		clear() {
+			this.newCardTitle = null;
+			this.createMode = false;
+		}
+	},
+
 }
 </script>
-<style scoped>
-.horizontal {
-	min-width: 251px;
-	max-width: 251px;
-}
-</style>
