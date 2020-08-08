2808b05cc6a9640c7657644bc06a2f60958bc852
Tássio Caique <tassiok2@gmail.com>
2020-08-08 00:11:13 -0300

    Add - Registra view de sprint nas rotas

--- resources/js/modules/board/routes/index.js
+++ resources/js/modules/board/routes/index.js
@@ -4,4 +4,9 @@ export default [
 		name: 'planning',
 		component: () => import('../views/Planning'),
 	},
+	{
+		path: '/sprint',
+		name: 'sprint',
+		component: () => import('../views/Sprint'),
+	},
 ];
