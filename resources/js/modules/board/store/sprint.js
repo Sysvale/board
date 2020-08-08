0387a1eb839f1c0cd45b4c15f57d261ce41ff4fc
Tássio Caique <tassiok2@gmail.com>
2020-08-08 00:13:15 -0300

    Add - Store de sprint

---
+++ resources/js/modules/board/store/sprint.js
@@ -0,0 +1,55 @@
+import { SPRINT_STORY_BOARD } from '../constants/defaultBoards';
+import upperCamelCase from '../../../core/utils/upperCamelCase';
+
+const initialState = () => {
+	let state = {};
+	SPRINT_STORY_BOARD.forEach(({ key }, index) => {
+		state = {
+			...state,
+			[key]: [],
+		}
+	});
+	return state;
+}
+
+const generateMutations = () => {
+	let mutations = {};
+	SPRINT_STORY_BOARD.forEach(({ key }) => {
+		let mutationName = `set${upperCamelCase(key)}`;
+		mutations = {
+			[mutationName]: (state, payload) => {
+				state[key] = payload;
+			},
+			...mutations,
+		};
+	});
+	return mutations;
+}
+
+const generateGetters = () => {
+	let getters = {};
+	SPRINT_STORY_BOARD.forEach(({ key }) => {
+		let getterName = `get${upperCamelCase(key)}`;
+		getters = {
+			[getterName]: (state) => {
+				return state[key];
+			},
+			...getters,
+		};
+	});
+	return getters;
+}
+
+
+export default {
+	namespaced: true,
+	state: {
+		...initialState(),
+	},
+	mutations: {
+		...generateMutations(),
+	},
+	getters: {
+		...generateGetters(),
+	}
+}
