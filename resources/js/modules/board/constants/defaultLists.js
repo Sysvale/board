7bfd909f5364052a83e3787d2b33561ea328dd4e
Tássio Caique <tassiok2@gmail.com>
2020-08-08 00:10:57 -0300

    Add - constantes para definições de lista padrão

--- resources/js/modules/board/constants/defaultLists.js
+++ resources/js/modules/board/constants/defaultLists.js
@@ -3,3 +3,9 @@ export const TEAM_ONE = 'team1';
 export const TEAM_TWO = 'team2';
 export const DEV_LOG = 'devlog';
 export const BUGS = 'bugs';
+
+export const TODO = 'todo';
+export const DEVELOPMENT = 'development';
+export const CODE_REVIEW = 'codeReview';
+export const DONE = 'done';
+export const DEPLOY = 'deploy';
