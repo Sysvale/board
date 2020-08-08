43502a4ef16dfc0c505631ef1c2e3f1c28a1d6b6
Tássio Caique <tassiok2@gmail.com>
2020-08-08 01:15:21 -0300

    Add - mockServices

--- resources/js/modules/board/services/index.js
+++ resources/js/modules/board/services/index.js
@@ -7,8 +7,11 @@ import {
 import {
 	USER_STORY,
 	USER_STORY_TASK,
+	DEV_TASK,
+	NOT_PLANNED_TASK,
 } from '../constants/cardCategories';
 
+//sprintBacklog makes more sense
 export const getUserStories = (teamId) => new Promise((resolve, reject) => {
 	setTimeout(() => {
 		const data = [
@@ -16,6 +19,7 @@ export const getUserStories = (teamId) => new Promise((resolve, reject) => {
 				id: '1',
 				title: 'User Story 1',
 				category: USER_STORY,
+				teamId: 'x',
 				tasks: [
 					{
 						id: '1a',
@@ -45,6 +49,7 @@ export const getUserStories = (teamId) => new Promise((resolve, reject) => {
 				id: '2',
 				title: 'User Story 2',
 				category: USER_STORY,
+				teamId: 'x',
 				tasks: [
 					{
 						id: '2a',
@@ -67,48 +72,60 @@ export const getUserStories = (teamId) => new Promise((resolve, reject) => {
 	}, 1000);
 });
 
-/*
-//plain
+export const getDevBacklog = (teamId) => new Promise((resolve, reject) => {
+	setTimeout(() => {
 		const data = [
 			{
-				id: '1',
-				title: 'User Story 1',
-				category: USER_STORY,
-			},
-			{
 				id: '1a',
-				title: 'Task A from User Story 1',
+				title: 'Dev task 1',
 				status: TODO,
 				cardId: '1',
-				category: USER_STORY_TASK,
+				category: DEV_TASK,
 			},
 			{
 				id: '1b',
-				title: 'Task B from User Story 1',
-				status: TODO,
+				title: 'Dev task 2',
+				status: DEVELOPMENT,
 				cardId: '1',
-				category: USER_STORY_TASK,
-			},
-
-			{
-				id: '2',
-				title: 'User Story 2',
-				category: USER_STORY,
+				category: DEV_TASK,
 			},
 			{
-				id: '2a',
-				title: 'Task A from User Story 2',
-				status: TODO,
-				cardId: '2',
-				category: USER_STORY_TASK,
-			},
-			{
-				id: '2b',
-				title: 'Task B from User Story 2',
-				status: TODO,
-				cardId: '2',
-				category: USER_STORY_TASK,
+				id: '1c',
+				title: 'Dev task 3',
+				status: DEVELOPMENT,
+				cardId: '1',
+				category: DEV_TASK,
 			},
 		];
+		resolve({data});
+	}, 1000);
+});
 
-*/
+export const getNotPlannedBacklog = (teamId) => new Promise((resolve, reject) => {
+	setTimeout(() => {
+		const data = [
+			{
+				id: '1a',
+				title: 'Not planned task 1',
+				status: TODO,
+				cardId: '1',
+				category: NOT_PLANNED_TASK,
+			},
+			{
+				id: '1b',
+				title: 'Not planned task 2',
+				status: DEVELOPMENT,
+				cardId: '1',
+				category: NOT_PLANNED_TASK,
+			},
+			{
+				id: '1c',
+				title: 'Not planned task 3',
+				status: DEVELOPMENT,
+				cardId: '1',
+				category: NOT_PLANNED_TASK,
+			},
+		];
+		resolve({data});
+	}, 1000);
+});
