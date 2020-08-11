e39f3f5b4e83735444ffd762c3c731d5469712e8
Tássio Caique <tassiok2@gmail.com>
2020-08-11 16:29:27 -0300

    Ref - refatoramento do store

--- resources/js/modules/board/store/index.js
+++ resources/js/modules/board/store/index.js
@@ -1,36 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import makeRequestStore from '../../../core/utils/makeRequestStore';
-
-import sprint from './sprint';
-import userStories from './userStories';
-import dev from './dev';
-import notPlanned from './notPlanned';
-
-import {
-	getLists,
-	getCardsByListsIds,
-} from '../services';
-
-const modules = [
-	{ getLists },
-	{ getCardsByListsIds },
-];
-
 Vue.use(Vuex);
 
+import userStories from './userStories';
+
 export default new Vuex.Store({
-	namespaced: true,
-	modules: {
-		...modules.reduce((acc, module) => ({
-			...acc,
-			...makeRequestStore(module, true),
-		}), {
-			sprint,
-			userStories,
-			dev,
-			notPlanned,
-		}),
-	},
+  namespaced: true,
+  modules: {
+    userStories,
+  }
 });
