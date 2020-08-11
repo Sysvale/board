b270c277cb3078ace09e2051020bbb34e315aed1
Tássio Caique <tassiok2@gmail.com>
2020-08-10 23:27:10 -0300

    Add - Card com modal para edição

--- resources/js/modules/board/components/Card.vue
+++ resources/js/modules/board/components/Card.vue
@@ -1,47 +1,77 @@
 <template>
-	<v-card
-		class="task-card"
-		v-bind="$attrs"
-		v-on="$listeners"
-		hover
-		@mouseover="hover = true"
-		@mouseleave="hover = false"
+	<v-dialog
+		v-if="$attrs.item"
+		v-model="dialog"
+		width="500"
 	>
-		<v-card-text
-			class="text--black"
-		>
-			<slot />
-		</v-card-text>
-		<v-card-actions
-			v-if="hover"
-		>
-			<v-btn
-				block
-				outlined
-				color="red"
-				small
-				@click="$emit('delete')"
-			>
-				Excluir
-			</v-btn>
-		</v-card-actions>
-	</v-card>
+		<template v-slot:activator="{}">
+				<v-card
+					class="task-card"
+					v-bind="$attrs"
+					v-on="$listeners"
+					hover
+					@click="showModal"
+					@mouseover="hover = true"
+					@mouseleave="hover = false"
+				>
+					<v-card-text
+						class="text--black"
+					>
+						<slot />
+					</v-card-text>
+				</v-card>
+		</template>
+		<v-card>
+			<v-container>
+				<v-layout>
+					<v-card-text
+						v-if="!titleInEditMode"
+						@click="titleInEditMode = true"
+					>
+						{{ $attrs.item.title }}
+					</v-card-text>
+					<v-textarea
+						v-else
+						v-model="$attrs.item.title"
+						solo
+						auto-grow
+						autofocus
+						@blur="handleSave"
+					/>
+				</v-layout>
+			</v-container>
+			<v-card-actions>
+				<v-btn
+					block
+					outlined
+					color="red"
+					small
+					@click="$emit('delete')"
+				>
+					Excluir
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
 </template>
 <script>
 export default {
 	data() {
 		return {
 			hover: false,
+			dialog: false,
+			titleInEditMode: false,
 		};
+	},
+
+	methods: {
+		showModal() {
+			this.dialog = true;
+		},
+		handleSave() {
+			this.titleInEditMode = false;
+			this.$emit('save');
+		}
 	},
 }
 </script>
-<style scoped>
-.task-card {
-	box-shadow: 0 3px 1px -2px rgba(0,0,0,.05),
-		0 2px 2px 0 rgba(0,0,0,.05),
-		0 1px 5px 0 rgba(0,0,0,.05) !important;
-	max-width: 250px;
-	min-width: 250px;
-}
-</style>
