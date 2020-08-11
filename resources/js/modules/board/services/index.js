7656e3881ca98fa758bf6d019eaf661a6063627e
Tássio Caique <tassiok2@gmail.com>
2020-08-10 23:30:10 -0300

    Dev - mock de services

--- resources/js/modules/board/services/index.js
+++ resources/js/modules/board/services/index.js
@@ -2,6 +2,7 @@ import http from '../../../http';
 import { 
 	DEVELOPMENT,
 	TODO,
+	BACKLOG,
 } from '../constants/defaultLists';
 
 import {
@@ -126,6 +127,84 @@ export const getNotPlannedBacklog = (teamId) => new Promise((resolve, reject) =>
 				category: NOT_PLANNED_TASK,
 			},
 		];
+		resolve({data});
+	}, 1000);
+});
+
+export const getLists = (boardId) => new Promise((resolve, reject) => {
+	setTimeout(() => {
+		const data = [
+			{
+				id: 'listId1',
+				name: 'List Name',
+				position: 1,
+			},
+			{
+				id: 'listId2',
+				name: 'List Name 1',
+				position: 2,
+			},
+			{
+				id: 'listId3',
+				name: 'List Name 2',
+				position: 3,
+			},
+			{
+				id: 'listId4',
+				name: 'List Name 3',
+				position: 4,
+			},
+		];
+
+		resolve({ data });
+	})
+});
+
+export const getCardsByListsIds = (listsIds) => new Promise((resolve, reject) => {
+	setTimeout(() => {
+		const mockedListsIds = [
+			'listId1',
+			'listId2',
+			'listId3',
+		];
+		const data = {
+			[mockedListsIds[0]]: [
+				{
+					id: '1a',
+					title: 'Not planned task 1',
+					category: NOT_PLANNED_TASK,
+				},
+			],
+			[mockedListsIds[1]]: [
+				{
+					id: '1b',
+					title: 'Not planned task 1',
+					category: NOT_PLANNED_TASK,
+				},
+			],
+			[mockedListsIds[2]]: [
+				{
+					id: '1c',
+					title: 'Not planned task 1',
+					category: NOT_PLANNED_TASK,
+				},
+				{
+					id: '1d',
+					title: 'Not planned task 1',
+					category: NOT_PLANNED_TASK,
+				},
+				{
+					id: '1e',
+					title: 'Not planned task 1',
+					category: NOT_PLANNED_TASK,
+				},
+				{
+					id: '1f',
+					title: 'Not planned task 1',
+					category: NOT_PLANNED_TASK,
+				},
+			],
+		};
 		resolve({data});
 	}, 1000);
 });
