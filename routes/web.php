632edfc69af42ad61fd5facf9c79c6c7ff9bc55e
Tássio Caique <tassiok2@gmail.com>
2020-08-20 17:30:28 -0300

    Add - Boilerplate de rotas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@
 
 Route::get('/', 'HomeController@index')->name('home');
 
+Route::get('/default-lists', 'BoardListController@getDefaultLists');
+Route::get('/planning-lists', 'BoardListController@getPlanningLists');
+
+Route::resource('cards', 'CardController')->only(['store', 'update', 'destroy']);
+Route::get('/get-cards-by-lists-ids', 'CardController@getCardsByListsIds');
+
+Route::resource('labels', 'LabelController')->only(['index']);
+Route::resource('members', 'MemberController')->only(['index']);
 
 Route::middleware('auth')->group(function() {
 
