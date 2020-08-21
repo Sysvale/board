464b9d9b606b776793e0de0be2790a1c60966a06
Tássio Caique <tassiok2@gmail.com>
2020-08-21 14:28:02 -0300

    Add - umock de service

--- resources/js/modules/board/services/sprint.js
+++ resources/js/modules/board/services/sprint.js
@@ -1,3 +1,5 @@
+import http from '../../../http';
+
 import generateUUID from '../../../core/utils/generateUUID';
 
 const id = () => generateUUID().split('-')[0];
@@ -9,38 +11,7 @@ const title = () => {
 	return splited[index] || "cachorro? sou cachorro não";
 }
 
-export const getDefaultLists = () => new Promise((resolve, reject) => {
-	setTimeout(() => {
-		const data = [
-			{
-				id: id(),
-				name: 'To Do',
-				position: 1,
-			},
-			{
-				id: id(),
-				name: 'Em desenvolvimento',
-				position: 2,
-			},
-			{
-				id: id(),
-				name: 'Code Review/Test',
-				position: 3,
-			},
-			{
-				id: id(),
-				name: 'Done/To Release',
-				position: 4,
-			},
-			{
-				id: id(),
-				name: 'Deploy',
-				position: 5,
-			},
-		];
-		resolve({ data });
-	})
-});
+export const getDefaultLists = () => http.get('/default-lists');
 
 export const getSprintDevlogLists = () => new Promise((resolve, reject) => {
 	setTimeout(() => {
