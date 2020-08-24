141e8e8c6676bfcb6621bfe067ea005cef9049e6
Tássio Caique <tassiok2@gmail.com>
2020-08-24 11:35:22 -0300

    Add - Keys para times

---
+++ app/Constants/TeamKeys.php
@@ -0,0 +1,10 @@
+<?php namespace App\Constants;
+
+use App\Library\Utils\BagOfConstants;
+
+class TeamKeys extends BagOfConstants
+{
+	const AVENGERS = 'avangers';
+	const STEPPER = 'stepper';
+	const BREAKOUT_ONE = 'breakoutOne';
+}
