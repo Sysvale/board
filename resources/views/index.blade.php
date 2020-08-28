0b78861d832048d78f97c7e74c6bee5ae44c5211
Tássio Caique <tassiok2@gmail.com>
2020-08-28 11:45:25 -0300

    Add - blade views

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,13 +7,9 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	@yield('css')
 </head>
 <body>
-  <div id="app">
-    <router-view></route-view>
-  </div>
-    {{-- <questions></questions> --}}
-  @yield('app-js')
+  <div id="app"></div>
+  <script src="{{ asset('js/app.min.js') }}" defer></script>
 </body>
 </html>
