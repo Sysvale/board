b7acc7d7b1f9751653c75c60257f32033dd9d9c5
Tássio Caique <tassiok2@gmail.com>
2020-08-30 11:21:52 -0300

    Dev - Boilerplate de listas na tela de planejamento

--- resources/js/modules/board/views/Planning.vue
+++ resources/js/modules/board/views/Planning.vue
@@ -3,11 +3,52 @@
 		fluid
 		class="px-2 py-5"
 	>
-		<board
-			namespace="planning"
-			:getLists="getPlanningLists"
-			:getCards="getCardsByListsIds"
-		/>
+		<v-expansion-panels
+			v-model="panels"
+			flat
+			multiple
+			tile
+			class="px-2"
+		>
+			<v-expansion-panel
+				key="buglogson"
+			>
+				<v-expansion-panel-header>
+					<div class="d-flex align-center">
+						<v-icon class="mr-2">
+							bug_report
+						</v-icon>
+						<h3 class="mb-0">Suporte, issues e outros problemas</h3>
+					</div>
+				</v-expansion-panel-header>
+				<v-expansion-panel-content>
+					<board
+						namespace="buglog"
+						:getLists="getPlanningLists"
+						:getCards="getCardsByListsIds"
+					/>
+				</v-expansion-panel-content>
+			</v-expansion-panel>
+			<v-expansion-panel
+				key="buglog"
+			>
+				<v-expansion-panel-header>
+					<div class="d-flex align-center">
+						<v-icon class="mr-2">
+							list
+						</v-icon>
+						<h3 class="mb-0">Planning</h3>
+					</div>
+				</v-expansion-panel-header>
+				<v-expansion-panel-content>
+					<board
+						namespace="planning"
+						:getLists="getPlanningLists"
+						:getCards="getCardsByListsIds"
+					/>
+				</v-expansion-panel-content>
+			</v-expansion-panel>
+		</v-expansion-panels>
 	</v-container>
 </template>
 
@@ -24,6 +65,11 @@ import {
 export default {
 	components: {
 		Board,
+	},
+	data() {
+		return {
+			panels: [1],
+		}
 	},
 	methods: {
 		getPlanningLists,
