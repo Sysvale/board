c302ae5f576f8c1e347783a6d418078f5f4bc4c0
Tássio Caique <tassiok2@gmail.com>
2020-08-30 15:46:41 -0300

    Add - Board de problemas

--- resources/js/modules/board/views/Planning.vue
+++ resources/js/modules/board/views/Planning.vue
@@ -11,33 +11,51 @@
 			class="px-2"
 		>
 			<v-expansion-panel
-				key="buglogson"
+				key="problems"
+				@change="handleProblemsPanelChange"
 			>
 				<v-expansion-panel-header>
 					<div class="d-flex align-center">
 						<v-icon class="mr-2">
 							bug_report
 						</v-icon>
-						<h3 class="mb-0">Suporte, issues e outros problemas</h3>
+						<h3 class="mb-0">Suporte, issues e débitos técnicos</h3>
 					</div>
 				</v-expansion-panel-header>
 				<v-expansion-panel-content>
+					<v-btn
+						:disabled="syncing"
+						:dark="!syncing"
+						color="#fc6d26"
+						class="mb-3"
+						depressed
+						small
+						outlined
+						@click.stop="syncing = true"
+					>
+						{{ syncing ? 'Sincronizando...' : 'Sincronizar com GitLab' }}
+					</v-btn>
+					<gitlab-synchronizer
+						v-if="syncing"
+						@finished="finishedHandler"
+					/>
 					<board
-						namespace="buglog"
-						:getLists="getPlanningLists"
+						v-if="wasSynced"
+						namespace="problems"
+						:getLists="getIssuesLists"
 						:getCards="getCardsByListsIds"
 					/>
 				</v-expansion-panel-content>
 			</v-expansion-panel>
 			<v-expansion-panel
-				key="buglog"
+				key="planning"
 			>
 				<v-expansion-panel-header>
 					<div class="d-flex align-center">
 						<v-icon class="mr-2">
 							list
 						</v-icon>
-						<h3 class="mb-0">Planning</h3>
+						<h3 class="mb-0">Planejamento de Sprints</h3>
 					</div>
 				</v-expansion-panel-header>
 				<v-expansion-panel-content>
@@ -54,9 +72,15 @@
 
 <script>
 import Board from '../components/Board.vue';
+import GitlabSynchronizer from '../components/GitlabSynchronizer';
+
 import {
 	getPlanningLists,
 } from '../services/planning';
+
+import {
+	getIssuesLists,
+} from '../services/issues';
 
 import {
 	getCardsByListsIds,
@@ -65,15 +89,35 @@ import {
 export default {
 	components: {
 		Board,
+		GitlabSynchronizer,
 	},
 	data() {
 		return {
 			panels: [1],
+			syncing: false,
+			wasSynced: true,
 		}
 	},
 	methods: {
 		getPlanningLists,
+		getIssuesLists,
 		getCardsByListsIds,
+
+		finishedHandler() {
+			this.syncing = false;
+			this.wasSynced = false;
+			this.$nextTick().then(() => {
+				this.wasSynced = true;
+			})
+		},
+
+		handleProblemsPanelChange(value) {
+			if(this.panels.indexOf(0) > -1) {
+				this.showSyncButton = true;
+				return;
+			}
+			this.showSyncButton = false;
+		}
 	},
 }
 </script>
