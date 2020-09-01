4cc085debc2a9704b240f4578fc6828448139782
Tássio Caique <tassiok2@gmail.com>
2020-09-01 17:28:23 -0300

    Add - Rota no front para chamada do resumo da sprint

--- resources/js/modules/board/services/sprint.js
+++ resources/js/modules/board/services/sprint.js
@@ -5,3 +5,5 @@ export const getDefaultLists = (params) => http.get('/lists/default', { params }
 export const getDevlogLists = (params) => http.get('/lists/devlog', { params });
 
 export const getPlanningLists = () => http.get('/lists/planning');
+
+export const getCurrentSprintSummaryByTeam = (teamId) => http.get(`/sprint/summary/current/${teamId}`);
