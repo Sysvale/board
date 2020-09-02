a32f171d558f6c76694890ab7fae2c47de1966a4
danielesreis <danielesreis@outlook.com>
2020-09-01 22:06:48 -0300

    Add biblioteca para comunicação com gitlab e handler

---
+++ app/Utils/GitlabHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Utils;
+
+use Gitlab;
+
+class GitlabHandler
+{
+	const LINK_FIELD = 'web_url';
+	const LABELS_FIELD = 'labels';
+	const TITLE_FIELD = 'title';
+
+	protected $client;
+
+	public function __construct()
+	{
+		$this->client = new Gitlab\Client();
+		$this->client->authenticate(env('GITLAB_TOKEN'), Gitlab\Client::AUTH_HTTP_TOKEN);
+	}
+
+	public function getAllIssues()
+	{
+		$pager = new Gitlab\ResultPager($this->client);
+
+		return collect($pager->fetchAll(
+			$this->client->issues(),
+			'all',
+			[env('GITLAB_PROJECT_ID'), ['state' => 'opened']]
+		));
+	}
+}
