9428bf18e99b942f5d18b29498915c40e0757e09
leolleocomp <leolleo.comp@gmail.com>
2020-09-04 13:31:32 -0300

    Add plugin do babel e atualiza backend do terraform

--- production/infra/backend.tf
+++ production/infra/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    skip_requesting_account_id  = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms      = true
     skip_metadata_api_check     = true
     endpoint                    = "https://sfo2.digitaloceanspaces.com"
     region                      = "us-east-1"
---
+++ production/infra/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+  required_version = ">= 0.13"
+}
