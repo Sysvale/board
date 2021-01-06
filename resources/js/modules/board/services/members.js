f51db4a0992e1c07862624ea9e109b3c45322c15
Tássio Caique <tassiok2@gmail.com>
2021-01-06 18:05:57 -0300

    Add - services e store de membros no front-end

--- resources/js/modules/board/services/members.js
+++ resources/js/modules/board/services/members.js
@@ -1,3 +1,9 @@
 import http from '../../../http';
 
 export const getMembers = () => http.get('/members');
+
+export const createMember = (payload) => http.post('/members', payload);
+
+export const updateMember = ({ id, ...params }) => http.put(`/members/${id}`, params);
+
+export const deleteMember = (id) => http.delete(`/members/${id}`);
