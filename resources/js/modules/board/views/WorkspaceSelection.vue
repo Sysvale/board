f8207538ce372d41aa234cb852a282248305eced
Tássio Caique <tassiok2@gmail.com>
2021-02-16 16:45:24 -0300

    Add - design da página inicial

--- resources/js/modules/board/views/WorkspaceSelection.vue
+++ resources/js/modules/board/views/WorkspaceSelection.vue
@@ -1,25 +1,35 @@
 <template>
 	<v-container
 		class="px-2 py-5"
+		fill-height
 	>
-			<v-row>
-				<v-col
-					v-for="workspace in workspaces"
-					:key="workspace.id"
-				>
-					<v-card
-						@click="goTo(workspace.id)"
+			<div class="justify-center align-center text-center w-100" style="margin: 0 auto">
+				<h1 class="text-center">Hello, baby!</h1>
+				<h2 class="text-center">Onde vamos trabalhar agora?</h2>
+				<v-row>
+					<v-col
+						v-for="workspace in workspaces"
+						:key="workspace.id"
 					>
-						<v-img
-							height="250"
-							src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-						></v-img>
-						<v-card-title>
-							{{ workspace.name }}
-						</v-card-title>
-					</v-card>
-				</v-col>
-			</v-row>
+						<div
+							class="workspace-card text-center px-5 py-5"
+							@click="goTo(workspace.id)"
+						>
+							<div
+								class="workspace-avatar"
+								style="background-image:url('https://cdn.vuetifyjs.com/images/cards/cooking.png')"
+							>
+							</div>
+							<div class="mt-5">
+								<h3>{{ workspace.name }}</h3>
+							</div>
+						</div>
+					</v-col>
+				</v-row>
+				<div class="mt-5">
+					<img src="/images/sysvale-logo.svg" />
+				</div>
+			</div>
 	</v-container>
 </template>
 
@@ -48,3 +58,18 @@ export default {
 	},
 }
 </script>
+<style scoped>
+.workspace-avatar {
+	border-radius: 50%;
+	width: 200px;
+	height: 200px;
+	margin: 0 auto;
+}
+
+.workspace-card:hover {
+	border: 1px solid #ccc;
+	cursor: pointer;
+	border-radius: 20px;
+	background: white;
+}
+</style>
