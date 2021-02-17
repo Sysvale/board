9bc11fe4d75b99365976810d903c94422459db88
Tássio Caique <tassiok2@gmail.com>
2021-02-17 12:25:35 -0300

    Add - redesign página inicial

--- resources/js/modules/board/views/WorkspaceSelection.vue
+++ resources/js/modules/board/views/WorkspaceSelection.vue
@@ -4,9 +4,16 @@
 		fill-height
 	>
 			<div class="justify-center align-center text-center w-100" style="margin: 0 auto">
-				<h1 class="text-center">Hello, baby!</h1>
-				<h2 class="text-center">Onde vamos trabalhar agora?</h2>
-				<v-row>
+				<div class="mb-5">
+					<img
+						src="/images/logo-dark.svg"
+						height="50px"
+						title="aka Trelindo"
+					/>
+				</div>
+				<h1 style="color: #888">Bão? Então tá bão!</h1>
+				<h2 style="color: #888">Onde vamos trabalhar agora?</h2>
+				<v-row class="py-10">
 					<v-col
 						v-for="workspace in workspaces"
 						:key="workspace.id"
@@ -15,11 +22,15 @@
 							class="workspace-card text-center px-5 py-5"
 							@click="goTo(workspace.id)"
 						>
-							<div
-								class="workspace-avatar"
-								style="background-image:url('https://cdn.vuetifyjs.com/images/cards/cooking.png')"
+							<lottie-player
+								:src="workspace.image"
+								background="white"
+								speed="1"
+								style="width: 250px; height: 250px;margin: 0 auto; background: white"
+								loop
+								autoplay
 							>
-							</div>
+							</lottie-player>
 							<div class="mt-5">
 								<h3>{{ workspace.name }}</h3>
 							</div>
