302369eb3ef33438457620becc0bfe5faecb67c2
Tássio Caique <tassiok2@gmail.com>
2021-02-17 12:23:05 -0300

    Fix - mock do service do workspace

--- resources/js/modules/board/services/workspaces.js
+++ resources/js/modules/board/services/workspaces.js
@@ -6,19 +6,16 @@ export const getWorkspaces = () => new Promise((resolve, reject) => {
       {
         id: 1,
         name: 'Produto',
+        image: 'https://assets5.lottiefiles.com/packages/lf20_jqc1jg5y.json',
         settings: {},
       },
       {
         id: 2,
+        image: 'https://assets6.lottiefiles.com/packages/lf20_ntigg4tl.json',
         name: 'Digestão',
         settings: {
           noPlanningProblems: true,
         },
-      },
-      {
-        id: 3,
-        name: 'Data itim',
-        settings: {},
       },
     ]});
 	}, 1000);
