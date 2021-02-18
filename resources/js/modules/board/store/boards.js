0fd43e042653dfbd6070edf494a1d0489ad580a5
Tássio Caique <tassiok2@gmail.com>
2021-02-18 15:26:21 -0300

    Rm - getter de workspace

--- resources/js/modules/board/store/boards.js
+++ resources/js/modules/board/store/boards.js
@@ -20,13 +20,6 @@ export default {
 	state: {
 		items: [],
 	},
-	getters: {
-		itemsByWorkspace(state, _, __, rootGetters) {
-			return state.items.filter(({ workspaceId }) => {
-				return workspaceId === rootGetters['workspaces/currentWorkspace'].id;
-			})
-		},
-	},
 	mutations: {
 		setItems(state, payload) {
 			state.items = convertKeysToCamelCase(payload);
