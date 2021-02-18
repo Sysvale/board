175503ceb801e03b31a5444d00d8d34a4e22a0e3
Tássio Caique <tassiok2@gmail.com>
2021-02-18 11:35:20 -0300

    Add - getter e mutations de currentWorkspace

--- resources/js/modules/settings/store/workspaces.js
+++ resources/js/modules/settings/store/workspaces.js
@@ -25,10 +25,23 @@ export default {
 	},
 	state: {
 		items: [],
+		selectedWorkpace: null,
+	},
+	getters: {
+		currentWorkspace(state) {
+			//TODO converKeysToCamelCase deeper
+			let copy = _.clone(state.selectedWorkpace);
+			if(!copy) return copy;
+			copy.settings = convertKeysToCamelCase(copy.settings);
+			return copy;
+		},
 	},
 	mutations: {
 		setItems(state, payload) {
 			state.items = convertKeysToCamelCase(payload);
+		},
+		setSelectedWorkspace(state, workspace) {
+			state.selectedWorkpace = workspace;
 		},
 	},
 }
