86aa43798f3c237b5edf1c2637fb0a31f9dc5b45
Tássio Caique <tassiok2@gmail.com>
2021-02-18 14:37:31 -0300

    Add - workspace_id no service

--- resources/js/modules/board/services/planning.js
+++ resources/js/modules/board/services/planning.js
@@ -1,5 +1,5 @@
 import http from '../../../http';
 
-export const getPlanningLists = () => http.get('/lists/planning');
+export const getPlanningLists = ({ workspace_id }) => http.get(`/lists/planning/${workspace_id}`);
 
 export const getProblemsLists = () => http.get('/lists/problems');
