0754ce2611b7e4906a5d55638d736f3dbee3d674
Tássio Caique <tassiok2@gmail.com>
2021-02-18 15:23:08 -0300

    Add - comando para setar labels, times e listas

--- app/Console/Commands/SetupManagementWorkspace.php
+++ app/Console/Commands/SetupManagementWorkspace.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands;
 
-use App\Constants\BoardListsKeys;
+use App\Constants\LabelKeys;
+use App\Constants\TeamKeys;
 use App\Models\BoardList;
+use App\Models\Label;
 use App\Models\Team;
 use Illuminate\Console\Command;
 
@@ -40,22 +42,121 @@ class SetupManagementWorkspace extends Command
 	 */
 	public function handle()
 	{
+		$this->info('Iniciando...');
+		$this->createLabels();
+		$this->createTeams();
 		$this->createSprintBoardLists();
+		$this->info('Tudo pronto!');
+	}
+
+	private function createLabels()
+	{
+		$managementLabels = [
+			[
+				'name' => 'Gente e Gestão',
+				'key' => LabelKeys::PM,
+				'color' => '#815AA5',
+				'text_color' => '#fff',
+			],
+			[
+				'name' => 'Financeiro',
+				'key' => LabelKeys::FINANCIAL,
+				'color' => '#028D29',
+				'text_color' => '#fff',
+			],
+			[
+				'name' => 'Comunicação',
+				'key' => LabelKeys::COMUNICATION,
+				'color' => '#FFAA2B',
+				'text_color' => 'rgba(0, 0, 0, 0.75)',
+			],
+	
+			[
+				'name' => 'Comercial',
+				'key' => LabelKeys::COMMERCIAL,
+				'color' => '#3171A3',
+				'text_color' => '#fff',
+			],
+			[
+				'name' => 'Projetos',
+				'key' => LabelKeys::PROJECTS,
+				'color' => '#54EF1A',
+				'text_color' => 'rgba(0, 0, 0, 0.75)',
+			],
+			[
+				'name' => 'Reunião',
+				'key' => LabelKeys::MEET,
+				'color' => '#EC6646',
+				'text_color' => '#fff',
+			],
+			[
+				'name' => 'Gestão de cidades',
+				'key' => LabelKeys::CITY_MANAGEMENT,
+				'color' => '#97D480',
+				'text_color' => 'rgba(0, 0, 0, 0.75)',
+			],
+	
+			[
+				'name' => 'Produto',
+				'key' => LabelKeys::PRODUCT,
+				'color' => '#21222E',
+				'text_color' => '#fff',
+			],
+		];
+
+		$this->info('Criando labels...');
+
+		
+		$value = Label::insert($managementLabels);
+
+		if ($value) {
+			$this->info('Labels inseridas com sucesso');
+		}
+	}
+
+	private function createTeams()
+	{
+		$teams = [
+			[
+				'name' => 'SysIN',
+				'key' => TeamKeys::SYS_IN,
+				'short_task_flow' => true,
+			],
+			[
+				'name' => 'SysOUT',
+				'key' => TeamKeys::SYS_OUT,
+				'short_task_flow' => true,
+			],
+		];
+
+		$this->info('Criando times...');
+
+		$value = Team::insert($teams);
+
+		if ($value) {
+			$this->info('Times inseridos com sucesso');
+		}
 	}
 
 	private function createSprintBoardLists()
 	{
 		$teams = Team::all();
+		
+		$this->info('Criando sprint board lists...');
 
 		$teams->each(function ($team) {
 			$board_list = BoardList::where('key', $team->key)->count();
 			if ($board_list === 0) {
-				BoardList::create([
+				$value = BoardList::create([
 				'name' => 'Sprint - '. $team->name,
 				'key' => $team->key,
 				'user_story_holder' => true,
 				'position' => 3,
 				]);
+
+				if ($value) {
+					$this->info('Lista criada com sucesso...');
+				}
 			}
 		});
 	}
