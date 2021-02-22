5a7f11f05bffbb9aadf353349869a5cf21653fa0
Jedson Melo <jedsonmeloc@gmail.com>
2021-02-22 11:18:59 -0300

    feat: seta propriedade para registrar que o senha foi configurada

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,28 +3,35 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reset Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller is responsible for handling password reset requests
+	| and uses a simple trait to include this behavior. You're free to
+	| explore this trait and override any methods you wish to tweak.
+	|
+	*/
 
-    use ResetsPasswords;
+	use ResetsPasswords;
 
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+	/**
+	 * Where to redirect users after resetting their password.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = RouteServiceProvider::HOME;
+
+	protected function setUserPassword($user, $password)
+	{
+		$user->configured_password = true;
+		$user->password = Hash::make($password);
+	}
 }
