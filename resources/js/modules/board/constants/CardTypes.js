2a19a66bf91a80e98c711bf6aaf0e9979b57ebc3
Jedson Melo <jedsonmeloc@gmail.com>
2021-03-11 15:02:05 -0300

    feat: add constantes para tipos de card

---
+++ resources/js/modules/board/constants/CardTypes.js
@@ -0,0 +1,9 @@
+export const USER_STORY = 'user-story';
+export const NOT_PRIORITIZED = 'not-prioritized';
+export const TASK = 'task';
+
+export default {
+	USER_STORY,
+	NOT_PRIORITIZED,
+	TASK,
+};
