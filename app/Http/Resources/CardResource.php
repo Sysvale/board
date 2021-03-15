ff49f813a36e6a4bd8c9b805fbdc1f5b8cb476ac
Jedson Melo <jedsonmeloc@gmail.com>
2021-03-15 10:48:57 -0300

    fix: adiciona campos corretos no resource do card

--- app/Http/Resources/CardResource.php
+++ app/Http/Resources/CardResource.php
@@ -17,6 +17,8 @@ class CardResource extends JsonResource
 		return [
 			'id' => $this->id,
 			'number' => $this->number,
+			'created_at' => $this->created_at->format('d/m/Y'),
+			'estimated' => $this->when(isset($this->estimated), $this->estimated),
 			'board_list_id' => $this->when(isset($this->board_list_id), $this->board_list_id),
 			'team_id' => $this->when(isset($this->team_id), $this->team_id),
 			'board_id' => $this->when(isset($this->board_id), $this->board_id),
@@ -31,11 +33,8 @@ class CardResource extends JsonResource
 			'workspace_id' => $this->when(isset($this->workspace_id), $this->workspace_id),
 			'description' => $this->when(isset($this->description), $this->description),
 			'rating' => $this->when(isset($this->rating), $this->rating),
-			'is_user_story' => $this->is_user_story,
-			'is_not_prioritized' => $this->is_not_prioritized,
-			'is_task' => $this->is_task,
-			'has_metric' => $this->has_metric,
-			'is_recurrent' => $this->is_recurrent,
+			'has_metric' => $this->when(isset($this->has_metric), $this->has_metric),
+			'is_recurrent' => $this->when(isset($this->is_recurrent), $this->is_recurrent),
 			'user' => $this->when(isset($this->user), $this->user),
 			'acceptance_criteria' => $this->when(
 				isset($this->acceptance_criteria),
