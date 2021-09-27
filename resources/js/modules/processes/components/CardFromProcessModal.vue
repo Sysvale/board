c8c4e5be0a0231dbc8bdfdbe38db98dfeee5b4a0
Tássio Caique Dias Freire <tassiok2@gmail.com>
2021-09-27 15:49:29 -0300

    feat: remove o v-menu e trabalha direto com o v-dialog

--- resources/js/modules/processes/components/CardFromProcessModal.vue
+++ resources/js/modules/processes/components/CardFromProcessModal.vue
@@ -1,111 +1,116 @@
 <template>
-	<v-menu
-		top
-		close-on-content-click
+	<v-dialog
+		v-model="dialog"
+		width="600"
 	>
 		<template v-slot:activator="{ on, attrs }">
 			<v-btn
-				block
 				small
 				depressed
 				:disabled="$attrs.loading"
-				color="white"
-				class="ml-2"
 				v-bind="attrs"
 				v-on="on"
+				icon
 			>
 				<v-icon
 					color="rgba(0, 0, 0, 0.5)"
 				>
-					more_vert
+					post_add
 				</v-icon>
 			</v-btn>
 		</template>
 
-		<v-list>
-			<v-list-item>
-				<v-dialog
-					v-model="dialog"
-					width="500"
-				>
-					<template v-slot:activator="{ on, attrs }">
-						<v-list-item-title
-							v-bind="attrs"
-							v-on="on"
+		<v-card>
+			<v-card-title class="text-h5">
+				{{ modalTitle }}
+			</v-card-title>
+
+			<v-card-text>
+				<checklist-from-process-select
+					:process-only="processOnly"
+					external-creation
+					form-first
+					@process-selected="selectedProcess = $event"
+					@checklist-selected="selectedChecklist = $event"
+				/>
+				<div>
+					<div
+						v-if="!processOnly && selectedChecklist"
+					>
+					  <div>Items da checklist</div>
+						<div class="text--grey caption mb-3">Essa checklist será adicionada automaticamente ao seu card</div>
+						<div
+							v-for="(checklistItem, i) in selectedChecklist.items"
+							:key="i"
+							class="px-2"
 						>
-							Criar a partir de um processo
-						</v-list-item-title>
-					</template>
-
-					<v-card>
-						<v-card-title class="text-h5 grey lighten-2">
-							Criação a partir de processos
-						</v-card-title>
-
-						<v-card-text>
-							<checklist-from-process-select
-								:process-only="processOnly"
-								external-creation
-								form-first
-								@process-selected="selectedProcess = $event"
-								@checklist-selected="selectedChecklist = $event"
-							/>
-							<div>
-								<v-expansion-panels
-									v-if="!!selectedProcess && selectedProcess.checklists.length > 0"
-									flat
-								>
-									<v-expansion-panel
-										v-for="(item, i) in selectedProcess.checklists"
-										:key="i"
-									>
-										<v-expansion-panel-header>
-											{{ item.title }}
-										</v-expansion-panel-header>
-										<v-expansion-panel-content>
-											<div
-												v-for="(checklistItem, j) in item.items"
-												:key="j"
-												class="px-2"
-											>
-												<div
-													class="d-flex"
-												>
-													<v-icon
-														small
-														class="mr-2"
-													>
-														check_box_outline_blank
-													</v-icon>
-													{{ checklistItem.description }}
-												</div>
-											</div>
-										</v-expansion-panel-content>
-									</v-expansion-panel>
-								</v-expansion-panels>
-							</div>
-						</v-card-text>
-
-						<v-divider></v-divider>
-
-						<v-card-actions>
-							<v-spacer></v-spacer>
-							<v-btn
-								color="primary"
-								text
-								@click="handleCreateClick"
+							<div
+								class="d-flex"
 							>
-								Criar
-							</v-btn>
-						</v-card-actions>
-					</v-card>
-				</v-dialog>
-			</v-list-item>
-		</v-list>
-	</v-menu>
+								<v-icon
+									small
+									class="mr-2"
+								>
+									check_box_outline_blank
+								</v-icon>
+								{{ checklistItem.description }}
+							</div>
+						</div>
+					</div>
+					<div
+						v-else-if="!!selectedProcess && selectedProcess.checklists.length > 0 && processOnly"
+					>
+						<div>Checklists</div>
+						<div class="text--grey caption mb-3">Cada uma das checklists abaixo virará uma tarefa para este item de backlog</div>
+						<v-expansion-panels
+							flat
+						>
+							<v-expansion-panel
+								v-for="(item, i) in selectedProcess.checklists"
+								:key="i"
+							>
+								<v-expansion-panel-header>
+									{{ item.title }}
+								</v-expansion-panel-header>
+								<v-expansion-panel-content>
+									<div
+										v-for="(checklistItem, j) in item.items"
+										:key="j"
+										class="px-2"
+									>
+										<div
+											class="d-flex"
+										>
+											<v-icon
+												small
+												class="mr-2"
+											>
+												check_box_outline_blank
+											</v-icon>
+											{{ checklistItem.description }}
+										</div>
+									</div>
+								</v-expansion-panel-content>
+							</v-expansion-panel>
+						</v-expansion-panels>
+					</div>
+				</div>
+			</v-card-text>
+
+			<v-card-actions>
+				<v-spacer></v-spacer>
+				<v-btn
+					color="primary"
+					text
+					@click="handleCreateClick"
+				>
+					Criar
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
 </template>
 <script>
-import { mapActions, mapMutations, mapState } from 'vuex';
 import ChecklistFromProcessSelect from './ChecklistFromProcessSelect.vue';
 export default {
   components: { ChecklistFromProcessSelect },
@@ -120,39 +125,26 @@ export default {
 			dialog: false,
 			selectedProcess: null,
 			selectedChecklist: null,
-			createMode: false,
 		};
 	},
 	computed: {
-		...mapState('processes', {
-			processes: state => state.items,
-			loading: ({ getProcesses }) => getProcesses.isFetching,
-		}),
+		modalTitle() {
+			if(this.processOnly) {
+				return 'Criar item de backlog a partir de um processo';
+			}
+			return 'Criar card a partir de uma checklist';
+		}
 	},
 
 	watch: {
-		createMode(value) {
-			if(value) {
-				this.fetchProcesses();
+		dialog(value) {
+			if(!value) {
+				this.selectedProcess = null;
+				this.selectedChecklist = null;
 			}
-		}
+		},
 	},
 	methods: {
-		isEmpty: (arg) => _.isEmpty(arg),
-		...mapActions('processes', [
-			'getProcesses',
-		]),
-
-		...mapMutations('processes', {
-			setProcesses: 'setItems',
-		}),
-
-		fetchProcesses() {
-			this.getProcesses().then((items) => {
-				this.setProcesses(items);
-			});
-		},
-
 		handleCreateClick() {
 			if(this.processOnly) {
 				this.$emit('create', this.selectedProcess);
