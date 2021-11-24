ef1601444ee93480739d2d72a1749b33c4a33b58
elayne.lemos <elayne.lemos@sysvale.com>
2021-11-24 10:49:37 -0300

    feat: change entrypoint

---
+++ .docker/bin/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./.docker/bin/apachelinker.sh /home/board/public
+
+chown www-data:$USER bootstrap/cache -R
+chown www-data storage -R
+
+if ! [ -d /tmp/dev.log ]; then
+	touch /tmp/dev.log
+fi
+
+tail -f /tmp/dev.log
