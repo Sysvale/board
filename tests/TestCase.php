72ee27957ee5beda368752630f78add46f76f066
uendel.couto <uendel.couto@gmail.com>
2022-03-31 15:28:21 -0300

    adiciona setup e teardown para que seja possível fazer testes no projeto

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,30 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use DB;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+	use CreatesApplication;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+	}
+
+	protected function tearDown(): void
+	{
+		$this->clearDB('mongodb');
+
+		parent::tearDown();
+	}
+
+	private function clearDB($connection): void
+	{
+		foreach (DB::connection($connection)->listCollections() as $coll) {
+			DB::connection($connection)
+				->table($coll->getName())
+				->delete();
+		}
+	}
 }
