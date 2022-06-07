5585755c6956aea5562a78910f437e4111708986
Tássio Caique <tassiok2@gmail.com>
2022-06-06 22:59:37 -0300

    feat: boilerplate da atualização de times

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Team;
 use App\Http\Resources\TeamResource;
+use Illuminate\Http\Request;
 
 class TeamController extends Controller
 {
 	public function index()
 	{
 		return TeamResource::collection(Team::get());
+	}
+
+	public function update(Request $request, Team $team)
+	{
+		$data = $request->validate([
+			'name' => 'required|string',
+			'board_lists' => 'required|array',
+		]);
+
+		$team->update($data);
+		$team->syncBoardLists($data['board_lists'] ?? []);
+		// atualizar board list sprint
+
+		return new TeamResource($team);
 	}
 }
