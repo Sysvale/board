d69476041ab1b613732abb3c8ee122a989beac58
Tássio Caique <tassiok2@gmail.com>
2022-06-06 23:04:45 -0300

    feat: adiciona método update, create e deleteMany

--- app/Services/BoardListService.php
+++ app/Services/BoardListService.php
@@ -43,4 +43,39 @@ class BoardListService
 
 		return $default_lists;
 	}
+
+	public function createMany($board_lists, $team_id)
+	{
+		if(empty($board_lists)) return;
+
+		foreach ($board_lists as $list) {
+			$list['team_id'] = $team_id;
+			BoardList::create($list);
+		}
+	}
+
+	public function deleteMany($board_lists_ids)
+	{
+		if(empty($board_lists_ids)) return;
+
+		foreach ($board_lists_ids as $id) {
+			$find = BoardList::find($id);
+			if($find) {
+				$find->delete();
+			}
+		}
+	}
+
+	public function updateMany($board_lists)
+	{
+		if(empty($board_lists)) return;
+
+		foreach ($board_lists as $list) {
+			$find = BoardList::find($list['id']);
+			if($find) {
+				$find->fill($list);
+				$find->save();
+			}
+		}
+	}
 }
