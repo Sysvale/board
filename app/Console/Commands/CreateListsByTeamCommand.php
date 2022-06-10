86e60d6e402a892f9b77d70d424f6b145e2a6fae
Tássio Caique <tassiok2@gmail.com>
2022-06-10 15:09:35 -0300

    feat: ajustes no log do comando e correção de comporamentos

--- app/Console/Commands/CreateListsByTeamCommand.php
+++ app/Console/Commands/CreateListsByTeamCommand.php
@@ -49,6 +49,8 @@ class CreateListsByTeamCommand extends Command
 	{
 		$this->info('Iniciando...');
 
+		$this->fixCodeReviewListKey();
+
 		$teams = Team::get();
 
 		$teams->each(function ($team) {
@@ -63,39 +65,64 @@ class CreateListsByTeamCommand extends Command
 			}
 		});
 
+		$teams->each(function ($team) {
+			$this->updateBoardListIdByListKey([$team->key . 'Dev'], $team, ['is_devlog' => true]);
+		});
+
 		
 		$this->removeOldLists();
 
 		$this->info('Finalizado!');
 	}
 
-	private function updateBoardListIdByListKey($list_keys, $team)
+	private function fixCodeReviewListKey()
+	{
+		$code_review = BoardList::where('name', 'Code Review')->get();
+
+		$code_review->each(function($item) {
+			$item->key = BoardListsKeys::CODE_REVIEW;
+			$item->save();
+		});
+	}
+
+	private function updateBoardListIdByListKey($list_keys, $team, $extra_data = [])
 	{
 		foreach($list_keys as $list_key) {
 			$current_board_list = BoardList::where('key', $list_key)->first();
-			$new_board_list = $this->createBoardList($current_board_list, $team);
+			if(empty($current_board_list)) continue;
+			$new_board_list = $this->createBoardList($current_board_list, $team, $extra_data);
 			$this->setupCardsInToNewBoardList($current_board_list, $new_board_list);
 		}
 	}
 
-	private function createBoardList($current_board_list, $team)
+	private function createBoardList($current_board_list, $team, $extra_data = [])
 	{
+		$this->info('-------------' . PHP_EOL);
+		$this->info("Replicando lista [" . $current_board_list->name . "] do time [" . $team->name . "]");
 		$this->addToRemoveList($current_board_list->id);
 		$new_board_list = $current_board_list->replicate();
 		$new_board_list->team_id = $team->id;
+		$new_board_list->fill($extra_data);
 		$new_board_list->save();
 		return $new_board_list;
 	}
-
+	
 	private function setupCardsInToNewBoardList($current_board_list, $new_board_list)
 	{
 		$cards = Card::where('board_list_id', $current_board_list->id)
 			->get();
 
-		$cards->each(function($card) use ($new_board_list) {
+		$this->info("Movendo [" . $cards->count() . "] cards da lista [" . $current_board_list->name . "] para [" . $new_board_list->name . "]" );
+
+		$bar = $this->output->createProgressBar($cards->count());
+
+		$cards->each(function($card) use ($new_board_list, $bar) {
 			$card->board_list_id = $new_board_list->id;
 			$card->save();
+			$bar->advance();
 		});
+
+		$this->info(PHP_EOL);
 	}
 
 	private function addToRemoveList($id)
@@ -105,7 +132,8 @@ class CreateListsByTeamCommand extends Command
 
 	private function removeOldLists()
 	{
-		$board_lists_to_remove = BoardList::whereIn('id', $this->board_lists_to_remove)->get();
+		$board_lists_to_remove = BoardList::whereIn('_id', $this->board_lists_to_remove)->get();
+		$this->info("Deletando " . $board_lists_to_remove->count() . " listas");
 		$board_lists_to_remove->each(function($list) {
 			$list->delete();
 		});
