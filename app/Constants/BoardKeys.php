98e22339f9ee4e9cdf83c77b1b661b2a3ed5f721
RafaelGondi <rdias.ga@gmail.com>
2022-08-28 11:40:12 -0300

    feat: Adiciona "Kaizen" como boardKey

--- app/Constants/BoardKeys.php
+++ app/Constants/BoardKeys.php
@@ -4,8 +4,9 @@ use App\Library\Utils\BagOfConstants;
 
 class BoardKeys extends BagOfConstants
 {
-    const NOT_PLANNED = 'notPlanned';
-    const IMPEDIMENTS = 'impediments';
-    const SPRINT_DEVLOG = 'sprintDevlog';
-    const SPRINT_BACKLOG = 'sprintBacklog';
+	const NOT_PLANNED = 'notPlanned';
+	const IMPEDIMENTS = 'impediments';
+	const SPRINT_DEVLOG = 'sprintDevlog';
+	const SPRINT_BACKLOG = 'sprintBacklog';
+	const KAIZEN = 'kaizen';
 }
