f1accb5ee84d14a18ed7a2d78373fe3c9cd69363
RafaelGondi <rdias.ga@gmail.com>
2022-08-28 18:46:30 -0300

    build: adiciona configuração no webpack.mix para compilar o v-calendar

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,40 +12,43 @@ const mix = require('laravel-mix');
  */
 
 module.exports = {
-  rules: [
-    {
-      test: /\.s(c|a)ss$/,
-      use: [
-        'vue-style-loader',
-        'css-loader',
-        {
-          loader: 'sass-loader',
-          // Requires sass-loader@^7.0.0
-          options: {
-            implementation: require('sass'),
-            fiber: require('fibers'),
-            indentedSyntax: true // optional
-          },
-          // Requires sass-loader@^8.0.0
-          options: {
-            implementation: require('sass'),
-            sassOptions: {
-              fiber: require('fibers'),
-              indentedSyntax: true // optional
-            },
-          },
-        },
-      ],
-    },
-  ],
+	rules: [
+		{
+			test: /\.s(c|a)ss$/,
+			use: [
+				'vue-style-loader',
+				'css-loader',
+				{
+					loader: 'sass-loader',
+					// Requires sass-loader@^7.0.0
+					options: {
+						implementation: require('sass'),
+						fiber: require('fibers'),
+						indentedSyntax: true, // optional
+					},
+					// Requires sass-loader@^8.0.0
+					options: {
+						implementation: require('sass'),
+						sassOptions: {
+							fiber: require('fibers'),
+							indentedSyntax: true, // optional
+						},
+					},
+				},
+			],
+		},
+	],
 }
 
 mix.babelConfig({
 	plugins: ['@babel/plugin-syntax-dynamic-import'],
 });
 
+require('laravel-mix-transpile-node-modules');
+mix.transpileNodeModules(['v-calendar']);
+
 mix.js('resources/js/modules/board/app.js', 'public/js/app.min.js')
 	.sass('resources/sass/app.scss', 'public/css')
-  .sass('resources/sass/home.scss', 'public/css');
+	.sass('resources/sass/home.scss', 'public/css');
 
 mix.version();
