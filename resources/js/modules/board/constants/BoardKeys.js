e32304bd4a19d4c9d3dd5e444df3214ba85865a5
RafaelGondi <rdias.ga@gmail.com>
2022-08-28 11:42:26 -0300

    feat: Adiciona "Kaizen" como constante nas chaves de board no front

--- resources/js/modules/board/constants/BoardKeys.js
+++ resources/js/modules/board/constants/BoardKeys.js
@@ -2,3 +2,4 @@ export const NOT_PLANNED = 'notPlanned';
 export const IMPEDIMENTS = 'impediments';
 export const SPRINT_DEVLOG = 'sprintDevlog';
 export const SPRINT_BACKLOG = 'sprintBacklog';
+export const KAIZEN = 'kaizen';
