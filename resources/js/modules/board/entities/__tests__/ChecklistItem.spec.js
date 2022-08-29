1a28b0369e3e6b248b7e5fcc4fa49b080aafbb67
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2022-08-29 15:27:40 -0300

    feat: Cria testes para classe de item de checklist

---
+++ resources/js/modules/board/entities/__tests__/ChecklistItem.spec.js
@@ -0,0 +1,32 @@
+import ChecklistItem from '../ChecklistItem';
+
+describe('ChecklistItem', () => {
+	test('if object structure is correct', () => {
+		const item = new ChecklistItem({ description: 'test' });
+
+		expect(item).toEqual({ description: 'test', done: false });
+	});
+
+	test('if class handles URL descriptions correctly', () => {
+		const itemWithUrl = new ChecklistItem({ description: 'www.google.com' });
+		const itemWithoutUrl = new ChecklistItem({ description: 'not an URL' });
+
+		expect(itemWithUrl.hasUrlDescription).toBeTruthy();
+		expect(itemWithoutUrl.hasUrlDescription).toBeFalsy();
+	});
+
+	test('if item content is formatted correctly', () => {
+		const itemWithHttpUrl = new ChecklistItem({ description: 'http://www.google.com' });
+		const itemWithCommonUrl = new ChecklistItem({ description: 'www.google.com' });
+		const itemWithoutUrl = new ChecklistItem({ description: 'not an URL' });
+
+		const expectedUrlDescription = {
+			url: 'http://www.google.com',
+			text: 'www.google.com',
+		};
+	
+		expect(itemWithHttpUrl.formattedUrlDescription).toMatchObject(expectedUrlDescription);
+		expect(itemWithCommonUrl.formattedUrlDescription).toMatchObject(expectedUrlDescription);
+		expect(itemWithoutUrl.formattedUrlDescription).toBe('not an URL');
+	});
+});
