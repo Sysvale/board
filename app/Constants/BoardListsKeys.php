18808c7d7c257173c8a85e3cb1d2fbb8112a487a
Tássio Caique <tassiok2@gmail.com>
2022-10-16 15:56:20 -0300

    rm: remove boardkeys relacionadas à integração

--- app/Constants/BoardListsKeys.php
+++ app/Constants/BoardListsKeys.php
@@ -10,15 +10,9 @@ class BoardListsKeys extends BagOfConstants
 	const CODE_REVIEW = 'codeReview';
 	const DONE = 'done';
 	const DEPLOY = 'deploy';
-	const BUGS = 'bugs';
-	const DEVLOG = 'devlog';
 	const BACKLOG = 'backlog';
-	const HELPDESK = 'helpDesk';
-	const DEVTASK = 'devtask';
 	const PO_REVIEW = 'poReview';
 	const REVIEWED = 'reviewed';
-	const DESIGN_SYSTEM = 'designSystem';
-	const SYS_SECURITY = 'sysSecurity';
 	const NOT_PRIORITIZED = 'notPrioritized';
 
 	const TODO_DT = 'todoDt';
@@ -43,15 +37,6 @@ class BoardListsKeys extends BagOfConstants
 	const EXTENDED_LISTS = [
 		self::PO_REVIEW,
 		self::REVIEWED
-	];
-
-	const DEFAULT_ISSUES_LISTS = [
-		self::BUGS,
-		self::HELPDESK,
-		self::DEVTASK,
-		self::DESIGN_SYSTEM,
-		self::SYS_SECURITY,
-		self::DEVLOG,
 	];
 
 	const DT_LISTS = [
