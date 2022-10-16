ceac3f7044ad4378770b3b05b5f561aff7184580
Tássio Caique <tassiok2@gmail.com>
2022-10-16 16:23:35 -0300

    rm: chamada à seeders no databaseseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,6 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		$this->call(
-			[
-				TeamSeeder::class,
-				BoardSeeder::class,
-				LabelSeeder::class,
-				BoardListSeeder::class,
-				MemberSeeder::class,
-			]
-		);
+		$this->call([]);
 	}
 }
