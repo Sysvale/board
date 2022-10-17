c7a910410a0cc9d18b2b3b3aa8563d46b0868a0a
rodrigo.sysvale <rodrigo.barboza@sysvale.com>
2022-10-17 17:37:45 -0300

    feat: cria factory para companies

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Company;
+use Faker\Generator as Faker;
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'cnpj' => $faker->regexify('[0-9]{15}'),
+        'name' => $faker->name(),
+        'phone' => $faker->phoneNumber(),
+        'email' => $faker->email(),
+    ];
+});
