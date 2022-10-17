9f42300adf02ef54aa942bbe203d1798bb8894db
henrique.franca <henrique.franca@sysvale.com>
2022-10-17 12:12:08 -0300

    feat: Cria company model

---
+++ app/Models/Company.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use SoftDeletes;
+
+	protected $fillable = [
+        'cnpj',
+        'name',
+        'phone',
+        'email',
+    ];
+
+
+    public function user()
+	{
+		return $this->hasMany(User::class);
+	}
+
+    public function workspace()
+	{
+		return $this->hasMany(Workspace::class);
+	}
+
+    public function teams()
+	{
+		return $this->hasMany(Team::class);
+	}
+}
+
+
+
