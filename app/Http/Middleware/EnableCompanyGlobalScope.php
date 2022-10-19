111ce8774c955e98bc0636e7f2b63d4a51503dd4
Edwildson C. Rodrigues <edwildson@hotmail.com>
2022-10-19 11:34:07 -0300

    feat: cria middleware de escopo

---
+++ app/Http/Middleware/EnableCompanyGlobalScope.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Card;
+use App\Models\Member;
+use App\Models\Scopes\CompanyScope;
+use App\Models\Team;
+use App\Models\Workspace;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class EnableCompanyGlobalScope
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check()){
+            $company_id = auth()->user()->member->company_id;
+
+            Card::addGlobalScope(new CompanyScope($company_id));
+            Member::addGlobalScope(new CompanyScope($company_id));
+            Team::addGlobalScope(new CompanyScope($company_id));
+            Workspace::addGlobalScope(new CompanyScope($company_id));
+        }
+        
+        return $next($request);
+    }
+}
