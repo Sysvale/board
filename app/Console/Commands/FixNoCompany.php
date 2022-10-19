8e3a59572242ebb601f5c6a63ed8243b9ddb927a
rodrigo.sysvale <rodrigo.barboza@sysvale.com>
2022-10-19 15:28:11 -0300

    feat: adiciona o model de card no comando e adiciona o company id

--- app/Console/Commands/FixNoCompany.php
+++ app/Console/Commands/FixNoCompany.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Card;
 use App\Models\Company;
 use App\Models\Member;
 use App\Models\Team;
@@ -52,50 +53,56 @@ class FixNoCompany extends Command
             )
         );
 
-        dump($company->name);
+        $this->info($company->name);
 
         $members_to_add_company = Member::where('company_id', null)->get();
 
         foreach ($members_to_add_company as $member) {
             if(!$member->company()->exists()) {
-                dump("Membro atualizado: $member->name");
-                $member->company()->associate($company->id);
+                $this->info("Membro atualizado: $member->name");
+                $member->company_id = $company->id;
                 $member->save();
             }
-                
         }
 
         $teams_to_add_company = Team::where('company_id', null)->get();
 
         foreach ($teams_to_add_company as $team) {
             if(!$team->company()->exists()) {
-                dump("Time atualizado: $team->name");
-                $team->company()->associate($company->id);
+                $this->info("Time atualizado: $team->name");
+                $team->company_id = $company->id;
                 $team->save();
             }
-                
         }
 
         $teams_to_add_company = TeamMember::where('company_id', null)->get();
 
         foreach ($teams_to_add_company as $team) {
             if(!$team->company()->exists()) {
-                dump("Time atualizado: $team->id");
-                $team->company()->associate($company->id);
+                $this->info("Time atualizado: $team->id");
+                $team->company_id = $company->id;
                 $team->save();
             }
-                
+        }
+
+        $cards_to_add_company = Card::where('company_id', null)->get();
+
+        foreach ($cards_to_add_company as $card) {
+            if(!$card->company()->exists()) {
+                $this->info("Card atualizado: $card->name");
+                $card->company_id = $company->id;
+                $card->save();
+            }
         }
 
         $workspaces_to_add_company = Workspace::where('company_id', null)->get();
 
         foreach ($workspaces_to_add_company as $workspace) {
             if(!$workspace->company()->exists()) {
-                dump("Workspace atualizado: $workspace->name");
-                $workspace->company()->associate($company->id);
+                $this->info("Workspace atualizado: $workspace->name");
+                $workspace->company_id = $company->id;
                 $workspace->save();
             }
-                
         }
     }
 }
