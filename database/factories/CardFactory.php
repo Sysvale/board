0c22a613db3e69175c4ad720adb0837e703b2c35
Edwildson C. Rodrigues <edwildson@hotmail.com>
2022-10-20 10:57:20 -0300

    feat: ajusta as fábricas

--- database/factories/CardFactory.php
+++ database/factories/CardFactory.php
@@ -2,6 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Models\BoardList;
 use App\Models\Card;
 use Faker\Generator as Faker;
 
@@ -11,5 +12,14 @@ $factory->define(Card::class, function (Faker $faker) {
 		'title' => $faker->sentence(),
 		'link' => $faker->url(),
 		'position' => $faker->numberBetween(1, 100),
+		'type' => 'user-story',
 	];
+});
+
+$factory->state(Card::class, 'with-board', function () {
+    return [
+        'board_list_id' => factory(BoardList::class)
+			->create()
+			->id,
+    ];
 });
---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Company;
+use App\Models\Team;
+use Faker\Generator as Faker;
+
+$factory->define(Team::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name(),
+        'board_lists' => [1,2],
+        'board_lists.0.name' => $faker->name(),
+        'board_lists.1.name' => $faker->name(),
+        'board_lists.0.position' => $faker->numberBetween(1,9),
+        'board_lists.1.position' => $faker->numberBetween(1,9),
+        'lottie_file' => $faker->url(),
+    ];
+});
+
+$factory->state(Team::class, 'with-company', function() {
+    return [
+        'company_id' => factory(Company::class)->create()->id,
+    ];
+});
