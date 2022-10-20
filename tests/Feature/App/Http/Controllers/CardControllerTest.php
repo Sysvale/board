0a3e695e36655cfcf58d0194ef114621f20d0085
rodrigo.sysvale <rodrigo.barboza@sysvale.com>
2022-10-20 12:08:37 -0300

    test: adiciona setUp com o usuário do teste

--- tests/Feature/App/Http/Controllers/CardControllerTest.php
+++ tests/Feature/App/Http/Controllers/CardControllerTest.php
@@ -8,6 +8,14 @@ use Tests\TestCase;
 
 class CardControllerTest extends TestCase
 {
+	public function setUp(): void
+	{
+		parent::setUp();
+		
+		$this->user = factory(User::class)->state('with-member-company')->create();
+		$this->actingAs($this->user);
+	}
+
 	public function testItIsPossibleToDestroyManyCardsInASingleRequest()
 	{
 		Card::unsetEventDispatcher();
@@ -26,17 +34,11 @@ class CardControllerTest extends TestCase
 
 	public function testIfAuthenticatedUsersCanCreateANewCard()
 	{
-		//Given we have an authenticated user
-		$this->actingAs(
-			factory(User::class)
-				->state('with-member-company')
-				->create()
-		);
-		//And a card object
 		$card = factory(Card::class)->state('with-board')->make();
-		//When user submits post request to create card endpoint
-		$result = $this->post('cards', $card->toArray());
-		//It gets stored in the database
+
+		$response = $this->post('cards', $card->toArray());
+
 		$this->assertEquals(1, Card::all()->count());
+		$this->assertEquals(201, $response->status());
 	}
 }
--- tests/Feature/App/Http/Controllers/MemberControllerTest.php
+++ tests/Feature/App/Http/Controllers/MemberControllerTest.php
@@ -2,32 +2,31 @@
 
 namespace Tests\Feature\App\Http\Controllers;
 
-use App\Models\Card;
 use App\Models\Member;
 use App\User;
 use Tests\TestCase;
 
 class MemberControllerTest extends TestCase
 {
+	public function setUp(): void
+	{
+		parent::setUp();
+		
+		$this->user = factory(User::class)->state('with-member-company')->create();
+		$this->actingAs($this->user);
+	}
+
 	public function testIfAuthenticatedMemberCanCreateANewMember()
 	{
-		//Given we have an authenticated user
-		$this->actingAs(
-			factory(User::class)
-				->state('with-member-company')
-				->create()
-		);
-		//And a card object
 		$member = factory(Member::class)->state('with-company')->make();
-
 		$data = $member->toArray();
 
 		$data = array_add($data, 'email', 'teste@email.com');
 		$data = array_add($data, 'team_ids', [1]);
 
-		//When user submits post request to create card endpoint
-		$result = $this->post('members', $data);
-		//It gets stored in the database
+		$response = $this->post('members', $data);
+
 		$this->assertEquals(2, Member::all()->count());
+		$this->assertEquals(201, $response->status());
 	}
 }
