c2335c0b7dc516c576d722a51f58b16885864e54
Tássio Caique <tassiok2@gmail.com>
2022-10-23 15:17:40 -0300

    feat: fallback para workspace sem lottiefile e ajustes no padrão de código

--- resources/js/modules/board/views/WorkspaceSelection.vue
+++ resources/js/modules/board/views/WorkspaceSelection.vue
@@ -3,55 +3,68 @@
 		class="px-2 py-5"
 		fill-height
 	>
-			<div class="justify-center align-center text-center w-100" style="margin: 0 auto">
-				<div class="mb-5">
-					<img
-						src="/images/logo-dark.svg"
-						height="50px"
-						title="aka Trelindo"
-					/>
-				</div>
-				<h1 style="color: #888">Bão? Então tá bão!</h1>
-				<h2 style="color: #888">Onde vamos trabalhar agora?</h2>
-				<v-row class="py-10">
-					<v-col
-						v-for="workspace in workspaces"
-						:key="workspace.id"
-					>
-						<div
-							class="workspace-card text-center px-5 py-5"
-							@click="goTo(workspace.id)"
-						>
-							<lottie-player
-								:src="workspace.lottieFile"
-								background="white"
-								speed="1"
-								style="width: 250px; height: 250px;margin: 0 auto; background: white"
-								loop
-								autoplay
-							>
-							</lottie-player>
-							<div class="mt-5">
-								<h3>{{ workspace.name }}</h3>
-							</div>
-						</div>
-					</v-col>
-				</v-row>
-				<div class="mt-5">
-					<img src="/images/sysvale-logo.svg" />
-				</div>
+		<div class="justify-center align-center text-center w-100" style="margin: 0 auto">
+			<div class="mb-5">
+				<img
+					src="/images/logo-dark.svg"
+					height="50px"
+					title="Trelássio"
+				>
 			</div>
+			<h1 style="color: #8B99A7">Onde vamos trabalhar agora?</h1>
+			<v-row class="pt-10">
+				<v-col
+					v-for="workspace in workspaces"
+					:key="workspace.id"
+				>
+					<div
+						class="workspace-card text-center px-5 py-5"
+						@click="goTo(workspace.id)"
+					>
+						<lottie-player
+							v-if="workspace.lottieFile"
+							:src="workspace.lottieFile"
+							background="white"
+							speed="1"
+							style="width: 250px; height: 250px;margin: 0 auto; background: white"
+							loop
+							autoplay
+						/>
+						<div
+							v-else
+							class="workspace-card-fallback"
+						>
+							{{ getWorkspaceAlias(workspace.name) }}
+						</div>
+						<div class="mt-5">
+							<h3>{{ workspace.name }}</h3>
+						</div>
+					</div>
+				</v-col>
+			</v-row>
+			<v-container
+				class="text-center pb-10"
+			>
+				<v-btn
+					text
+					depressed
+					outlined
+					@click="goToCompanyPlanning"
+				>
+					Workspace geral
+				</v-btn>
+			</v-container>
+			<div class="mt-5">
+				<img src="/images/sysvale-logo.svg">
+			</div>
+		</div>
 	</v-container>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from 'vuex';
 
 export default {
-	data() {
-		return {
-		}
-	},
 	computed: {
 		...mapState('workspaces', {
 			workspaces: 'items',
@@ -66,21 +79,40 @@ export default {
 				},
 			});
 		},
+		goToCompanyPlanning() {
+			this.$router.push({
+				name: 'workspace.company',
+			});
+		},
+		getWorkspaceAlias(name) {
+			if (!name) return 'WS';
+			if (name.split(' ').length > 1) {
+				return `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`
+					.toUpperCase();
+			}
+			return `${name[0]}${name[1] || ''}`.toUpperCase();
+		},
 	},
-}
+};
 </script>
 <style scoped>
-.workspace-avatar {
-	border-radius: 50%;
-	width: 200px;
-	height: 200px;
-	margin: 0 auto;
-}
-
 .workspace-card:hover {
 	border: 1px solid #ccc;
 	cursor: pointer;
 	border-radius: 20px;
 	background: white;
+}
+
+.workspace-card-fallback {
+	background: #EFF5FB;
+	color: #8B99A7;
+	font-size: 100px;
+	display: flex;
+	justify-content: center;
+	justify-items: center;
+	align-items: center;
+	width: 250px;
+	height: 250px;
+	margin: 0 auto;
 }
 </style>
