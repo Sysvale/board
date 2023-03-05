2ead75c9bcbbb952ea2d803de4b46de46ae1efa2
RafaelGondi <rdias.ga@gmail.com>
2023-03-05 18:59:08 -0300

    feat: adiciona no cabeçalho a velocidade do time

--- resources/js/modules/reports/views/SprintReport.vue
+++ resources/js/modules/reports/views/SprintReport.vue
@@ -29,15 +29,20 @@
 						<v-toolbar-title>Sprints</v-toolbar-title>
 						<v-spacer />
 					</v-toolbar>
+
+					<div class="mb-4">
+						<small class="pl-4">Velocidade do time: </small>
+						<small class="pl-2 font-weight-bold">{{ getTeamVelocity() }} pts</small>
+					</div>
 				</template>
 				<template v-slot:item.period="{ item }">
 					<div>
 						{{ getSprintPeriod(item) }}
 					</div>
 				</template>
-				<template v-slot:item.velocity="{ item }">
+				<template v-slot:item.deliveredPoints="{ item }">
 					<div>
-						{{ getSprintVelocity(item) }} pts
+						{{ getDeliveredPoints(item) }} pts
 					</div>
 				</template>
 				<template v-slot:item.actions="{ item }">
@@ -78,7 +83,7 @@ export default {
 	data() {
 		return {
 			dialog: false,
-			teamVelocity: [],
+			teamVelocity: 0,
 			headers: [
 				{
 					text: 'Data',
@@ -87,10 +92,10 @@ export default {
 					value: 'period',
 				},
 				{
-					text: 'Velocidade',
+					text: 'Pontos entregues',
 					align: 'start',
 					sortable: true,
-					value: 'velocity',
+					value: 'deliveredPoints',
 				},
 				{ text: 'Ações', value: 'actions', sortable: false },
 			],
@@ -158,8 +163,8 @@ export default {
 			return `${moment(item.startedAt).format('DD/MM/yy')} à ${moment(item.finishedAt).format('DD/MM/yy')}`;
 		},
 
-		getSprintVelocity(item) {
-			const velocity = item.cards.reduce((acc, card) => {
+		getDeliveredPoints(item) {
+			const deliveredPoints = item.cards.reduce((acc, card) => {
 				if (card.status !== 'done') {
 					return acc + 0;
 				}
@@ -167,7 +172,15 @@ export default {
 				return acc + Number(card.estimated);
 			}, 0);
 
-			return velocity;
+			return deliveredPoints;
+		},
+
+		getTeamVelocity() {
+			const numberOfSprints = this.sprints.length;
+			const velocity = this.sprints.reduce((acc, curr) => acc + this.getDeliveredPoints(curr), 0);
+
+			// eslint-disable-next-line no-bitwise
+			return ~~(velocity / numberOfSprints);
 		},
 	},
 };
