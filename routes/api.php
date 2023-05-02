59c6ab6fb015b89975b16b23f9d711b723969a60
uesley <uesley.eumesmo@gmail.com>
2023-05-02 12:02:41 -0300

    cria rota para criar card via api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CardController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('cards', [CardController::class, 'store'])->name('api.cards.store');
