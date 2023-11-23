fd75f0000cc9e7e6f41ff33cb6bd4d49aa816ab8
Geidson Benicio Coelho <geidsonc@gmail.com>
2023-11-22 21:47:58 -0300

    Atualiza declaração de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,20 @@
 <?php
 
+use App\Http\Controllers\BacklogLabelController;
+use App\Http\Controllers\BoardController;
+use App\Http\Controllers\BoardListController;
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\GoalController;
+use App\Http\Controllers\LabelController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\MilestoneController;
+use App\Http\Controllers\ProcessController;
+use App\Http\Controllers\SprintReportController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WorkspaceController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
@@ -19,72 +34,64 @@ Auth::routes([
 	'register' => false,
 ]);
 
-$any = \App\Constants\RouteConstants::ANY;
 Route::redirect('/', 'login');
 
 Route::group(
 	['middleware' => ['auth']], function () {
-		Route::get('/lists/default', 'BoardListController@getTaskLists');
-		Route::get('/lists/planning/company', 'BoardListController@getCompanyPlanningLists');
-		Route::get('/lists/planning/{workspace}', 'BoardListController@getPlanningLists');
-		Route::get('/lists/devlog', 'BoardListController@getDevlogLists');
+		Route::get('/lists/default', [BoardListController::class, 'getTaskLists']);
+		Route::get('/lists/planning/company', [BoardListController::class, 'getCompanyPlanningLists']);
+		Route::get('/lists/planning/{workspace}', [BoardListController::class, 'getPlanningLists']);
+		Route::get('/lists/devlog', [BoardListController::class, 'getDevlogLists']);
 
-		Route::delete('cards/delete-many', 'CardController@destroyMany')
+		Route::delete('cards/delete-many', [CardController::class, 'destroyMany'])
 			->name('cards.destroy_many');
-		Route::resource('cards', 'CardController')->only(['store', 'update', 'destroy']);
-		Route::get(
-			'/cards/impediments/{team}',
-			'CardController@getImpedimentsByTeam'
-		);
+		Route::resource('cards', CardController::class)->only(['store', 'update', 'destroy']);
+		Route::get('/cards/impediments/{team}', [CardController::class, 'getImpedimentsByTeam']);
+		Route::get('/cards/from-user-story', [CardController::class, 'getTaskCardsFromUserStory']);
+		Route::get('/cards/from-devlog', [CardController::class, 'getTaskCardsFromDevlog']);
+		Route::get('/cards/from-not-planned', [CardController::class, 'getTaskCardsFromNotPlanned']);
+		Route::get('/cards/from-kaizen', [CardController::class, 'getTaskCardsFromKaizen']);
+		Route::get('/cards/from-company-planning', [CardController::class, 'getCompanyPlanningCards']);
+		Route::get('/cards/from-planning', [CardController::class, 'getPlanningCards']);
+		Route::post( '/cards/update-positions', [CardController::class, 'updateCardsPositions']);
 
-		Route::get('/cards/from-user-story', 'CardController@getTaskCardsFromUserStory');
-		Route::get('/cards/from-devlog', 'CardController@getTaskCardsFromDevlog');
-		Route::get('/cards/from-not-planned', 'CardController@getTaskCardsFromNotPlanned');
-		Route::get('/cards/from-kaizen', 'CardController@getTaskCardsFromKaizen');
-		Route::get('/cards/from-company-planning', 'CardController@getCompanyPlanningCards');
-		Route::get('/cards/from-planning', 'CardController@getPlanningCards');
+		Route::get('/user-stories/{team}', [CardController::class, 'getUserStoriesByTeam']);
 
-		Route::post(
-			'/cards/update-positions',
-			'CardController@updateCardsPositions'
-		);
-		Route::get('/user-stories/{team}', 'CardController@getUserStoriesByTeam');
-		Route::get(
-			'/sprint/summary/current/{team}',
-			'CardController@getCurrentSprintSummaryByTeam'
-		);
-		Route::resource('labels', 'LabelController');
-		Route::get('/labels/workspace/{workspace}', 'LabelController@getLabelsByWorkspaceId');
-		Route::resource('members', 'MemberController');
-		Route::resource('/teams', 'TeamController');
-		Route::get('/boards', 'BoardController@index');
+        Route::get( '/sprint/summary/current/{team}', [CardController::class, 'getCurrentSprintSummaryByTeam']);
 
-		Route::resource('events', 'EventController')->only(['store', 'update', 'destroy']);
-		Route::get('/events/{team}', 'EventController@getEventsByTeam');
+        Route::resource('labels', LabelController::class);
+		Route::get('/labels/workspace/{workspace}', [LabelController::class, 'getLabelsByWorkspaceId']);
 
-		Route::resource('goals', 'GoalController');
+		Route::resource('members', MemberController::class);
+		Route::resource('/teams', TeamController::class);
+		Route::get('/boards', [BoardController::class, 'index']);
+
+		Route::resource('events', EventController::class)->only(['store', 'update', 'destroy']);
+		Route::get('/events/{team}', [EventController::class, 'getEventsByTeam']);
+
+		Route::resource('goals', GoalController::class);
 
 		Route::get('/logout', function () {
 			Auth::logout();
 			return redirect('login');
 		});
 
-		Route::apiResource('workspaces', 'WorkspaceController');
-		Route::apiResource('processes', 'ProcessController');
-		Route::apiResource('milestones', 'MilestoneController');
-		Route::apiResource('sprint-reports', 'SprintReportController')->only(['store']);
-		Route::get('sprint-reports/{team}', 'SprintReportController@getSprintReportByTeamId');
+		Route::apiResource('workspaces', WorkspaceController::class);
+		Route::apiResource('processes', ProcessController::class);
+		Route::apiResource('milestones', MilestoneController::class);
+		Route::apiResource('sprint-reports', SprintReportController::class)->only(['store']);
+		Route::get('sprint-reports/{team}', [SprintReportController::class, 'getSprintReportByTeamId']);
 
 
-		Route::post('users/resend-welcome-mail', 'UserController@resendWelcomeMail');
+		Route::post('users/resend-welcome-mail', [UserController::class, 'resendWelcomeMail']);
 
-		Route::get('/reports/sprint-overview/{team}', 'SprintReportController@getCurrentSprintOverviewByTeam');
+		Route::get('/reports/sprint-overview/{team}', [SprintReportController::class, 'getCurrentSprintOverviewByTeam']);
 
-		Route::resource('backlog-labels', 'BacklogLabelController');
+		Route::resource('backlog-labels', BacklogLabelController::class);
 
-		Route::get('milestones/{milestone}/backlog-items/not-started', 'MilestoneController@getNotStarted');
-		Route::get('milestones/{milestone}/backlog-items/on-going', 'MilestoneController@getOnGoing');
-		Route::get('milestones/{milestone}/backlog-items/finished', 'MilestoneController@getFinished');
+		Route::get('milestones/{milestone}/backlog-items/not-started', [MilestoneController::class, 'getNotStarted']);
+		Route::get('milestones/{milestone}/backlog-items/on-going', [MilestoneController::class, 'getOnGoing']);
+		Route::get('milestones/{milestone}/backlog-items/finished', [MilestoneController::class, 'getFinished']);
 	}
 );
 
