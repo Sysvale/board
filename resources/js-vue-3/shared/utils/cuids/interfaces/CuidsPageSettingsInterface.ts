90fbe147e5be50dd6e89235eff1856f8329e8244
Tássio Caique Dias Freire <tassiok2@gmail.com>
2023-12-06 13:34:32 -0300

    feat: implementa interface com atributos para configuração de páginas cuids

---
+++ resources/js-vue-3/shared/utils/cuids/interfaces/CuidsPageSettingsInterface.ts
@@ -0,0 +1,21 @@
+export interface ICuidsPageSettings {
+	addItemButtonText: String;
+	createSuccessFeedbackTitle: String;
+	createSuccessFeedbackText: String;
+	createSuccessFeedbackParams: Object;
+	deleteSuccessFeedbackText: String;
+	deleteSuccessFeedbackTitle: String;
+	deleteSuccessFeedbackParams: Object;
+	deleteConfirmationTitle: String;
+	deleteConfirmationText: String;
+	emptyStateImage: String;
+	emptyStateDescription: String;
+	emptyStateTitle: String;
+	emptyStateText: String;
+	emptyStateActionButtonText: String;
+	pageTitle: String;
+	pageSubtitle: String;
+	updateSuccessFeedbackText: String;
+	updateSuccessFeedbackTitle: String;
+	updateSuccessFeedbackParams: Object;
+}
